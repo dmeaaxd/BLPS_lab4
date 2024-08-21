d427074aadc923af435aefddca8565217d84dbd1
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-08-21 15:14:34 +0300

    Added necessary dependencies, connected PostgreSQL DB

--- src/main/java/ru/danmax/BlpsLab4Application.java
+++ src/main/java/ru/danmax/BlpsLab4Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
-  public static void main(String... args) {
-    SpringApplication.run(Application.class, args);
-  }
+public class BlpsLab4Application {
+    public static void main(String... args) {
+        SpringApplication.run(BlpsLab4Application.class, args);
+    }
 
 }
